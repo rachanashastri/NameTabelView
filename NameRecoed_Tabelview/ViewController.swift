6115c8f5bba935e1362652eab11b2726c9ddf5aa
Ronak Shastri <ronak.shastri@verizonmedia.com>
2022-04-18 15:01:58 -0400

    Initial Commit

---
+++ NameRecoed_Tabelview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NameRecoed_Tabelview
+//
+//  Created by Ronak Shastri on 2022-04-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
